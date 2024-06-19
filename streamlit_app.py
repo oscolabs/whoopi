cd3f58df727837b1b2ce58feb7e62e7247cbfb48
oscolabs <68839329+oscolabs@users.noreply.github.com>
2024-06-18 21:31:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.title('🎫 Whoopi')
 st.info('To write a ticket, fill out the form below. Check status or review ticketing analytics using the tabs below.')
 
 client_id='07fa9a7e-6e68-4ca1-aa03-e535ca5f7816'
-client_secret= 'f488d57fb86082c1125b565a36375ba9c63e3768c2fe04a82c3bff6b1097fbb8
+client_secret= 'f488d57fb86082c1125b565a36375ba9c63e3768c2fe04a82c3bff6b1097fbb8'
 
 def get_access_token(url, client_id, client_secret):
     response = requests.post(

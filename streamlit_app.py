c8ac32dc10bd4dc8a87225d5919e98eeefa6119a
oscolabs <68839329+oscolabs@users.noreply.github.com>
2024-06-19 15:51:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,45 +16,15 @@ import json
 from requests import post
 import urllib.parse
 
-
-client_id='07fa9a7e-6e68-4ca1-aa03-e535ca5f7816'
-client_secret= 'f488d57fb86082c1125b565a36375ba9c63e3768c2fe04a82c3bff6b1097fbb8'
-
-client_id = urllib.parse.quote(client_id.encode('utf8'))
-client_secret = urllib.parse.quote(client_secret.encode('utf8'))
-
-code_bytes = f"{client_id}:{client_secret}".encode('ascii')
-base64_bytes = base64.b64encode(code_bytes)
-base64_code = base64_bytes.decode('ascii')
-
-url1 = 'https://api.prod.whoop.com/oauth/oauth2/auth'
-url2 = 'https://api.prod.whoop.com/oauth/oauth2/token'
-
-
-headers ={
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
-    'Content-Type': "application/x-www-form-urlencoded",
-    'Authorization': f"Basic {base64_code}"
-}
-
-data = {   
-    "grant_type" : "authorization_code",
-    'code': authorization_code , 
-    'redirect_uri': redirect_uri
-}
-
-response = requests.post(url1, data=data, headers=headers)
-print(response)
-print(response.text)
-
-print(client_id, client_secret)
+client_id = '08627f9b960141dd944cd5845ab082f7'
+client_secret = 'bb2da36aa93f463093fb2a9402cde74a'
 
 def get_token():
     auth_string = client_id + ':' + client_secret
     auth_bytes = auth_string.encode('utf-8')
     auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
 
-    url = 'https://api.prod.whoop.com/oauth/oauth2/auth'
+    url = 'https://accounts.spotify.com/api/token'
     headers = {
         'Authorization': 'Basic ' + auth_base64,
         'Content-Type': 'application/x-www-form-urlencoded'
@@ -65,11 +35,62 @@ def get_token():
     token = json_result['access_token']
     return token 
 
-# token = get_token()
-# st.write(token)
+st.write(get_token)
 
-def get_auth_header(token):
-    return {"Authorization": "Bearer " + token}
+
+# client_id='07fa9a7e-6e68-4ca1-aa03-e535ca5f7816'
+# client_secret= 'f488d57fb86082c1125b565a36375ba9c63e3768c2fe04a82c3bff6b1097fbb8'
+
+# client_id = urllib.parse.quote(client_id.encode('utf8'))
+# client_secret = urllib.parse.quote(client_secret.encode('utf8'))
+
+# code_bytes = f"{client_id}:{client_secret}".encode('ascii')
+# base64_bytes = base64.b64encode(code_bytes)
+# base64_code = base64_bytes.decode('ascii')
+
+# url1 = 'https://api.prod.whoop.com/oauth/oauth2/auth'
+# url2 = 'https://api.prod.whoop.com/oauth/oauth2/token'
+
+
+# headers ={
+#     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
+#     'Content-Type': "application/x-www-form-urlencoded",
+#     'Authorization': f"Basic {base64_code}"
+# }
+
+# data = {   
+#     "grant_type" : "authorization_code",
+#     'code': authorization_code , 
+#     'redirect_uri': redirect_uri
+# }
+
+# response = requests.post(url1, data=data, headers=headers)
+# print(response)
+# print(response.text)
+
+# print(client_id, client_secret)
+
+# def get_token():
+#     auth_string = client_id + ':' + client_secret
+#     auth_bytes = auth_string.encode('utf-8')
+#     auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+#     url = 'https://api.prod.whoop.com/oauth/oauth2/auth'
+#     headers = {
+#         'Authorization': 'Basic ' + auth_base64,
+#         'Content-Type': 'application/x-www-form-urlencoded'
+#     }
+#     data = {'grant_type': 'client_credentials'}
+#     result = post(url, headers=headers, data=data)
+#     json_result = json.loads(result.content)
+#     token = json_result['access_token']
+#     return token 
+
+# # token = get_token()
+# # st.write(token)
+
+# def get_auth_header(token):
+#     return {"Authorization": "Bearer " + token}
 
 # client_id='07fa9a7e-6e68-4ca1-aa03-e535ca5f7816'
 # client_secret= 'f488d57fb86082c1125b565a36375ba9c63e3768c2fe04a82c3bff6b1097fbb8'

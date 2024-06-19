45ecdcd605727394f032f9493d78fa8733df4b5e
oscolabs <68839329+oscolabs@users.noreply.github.com>
2024-06-18 21:33:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,7 @@ st.info('To write a ticket, fill out the form below. Check status or review tick
 
 client_id='07fa9a7e-6e68-4ca1-aa03-e535ca5f7816'
 client_secret= 'f488d57fb86082c1125b565a36375ba9c63e3768c2fe04a82c3bff6b1097fbb8'
+url = 'https://api.prod.whoop.com/oauth/oauth2/auth'
 
 def get_access_token(url, client_id, client_secret):
     response = requests.post(
@@ -18,9 +19,9 @@ def get_access_token(url, client_id, client_secret):
         data={"grant_type": "client_credentials"},
         auth=(client_id, client_secret),
     )
-    return response.json()["access_token"]
+    st.write(response.json()["access_token"])
 
-st.write(response.json()["access_token"])
+#st.write(response.json()["access_token"])
 # get_access_token("https://api.example.com/access_token", "abcde", "12345")
 
 # # Generate data
